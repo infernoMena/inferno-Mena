739f620a5ba6bf0a2acf55ec08dd58df6d7ec179
infernoMena <e0ec78b7da@gmail.com>
2025-04-10 20:16:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// script.js
-
+// عند تحميل الصفحة
 document.addEventListener("DOMContentLoaded", function () {
+  // زر الديسكورد
   const discordBtn = document.querySelector(".discord-btn");
 
+  // حدث عند الضغط على الزر
   discordBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    const go = confirm("🔥 هل أنت جاهز للانضمام إلى مجتمع Inferno؟");
+    e.preventDefault(); // نمنع الرابط من الفتح الفوري
+
+    // رسالة تأكيد للمستخدم
+    const go = confirm("🔥 هل أنت مستعد تنضم إلى مجتمع CrossFire؟");
+
+    // إذا وافق، نفتح الرابط
     if (go) {
       window.open(this.href, "_blank");
     }
